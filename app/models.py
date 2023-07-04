19eafcca24db655236390af6f2121f489a51ff8f
yeh-sudo <yehchanshen@gmail.com>
2023-07-04 17:19:56 +0800

    create restaurant model and user model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, Field
+from typing import List
 
 class Rating(BaseModel):
     cleanliness: int = Field(...)
@@ -6,6 +7,27 @@ class Rating(BaseModel):
     deliciousness: int = Field(...)
     CPR: int = Field(...)
     overall: int = Field(...)
+    
 class Comment(BaseModel):
     rating: Rating
     content: str = Field(...)
+
+class Restaurant(BaseModel):
+    name: str
+    comments_id: List[str]
+    star: float
+    tags: str
+    open_hour: str
+    address: str
+    phone_number: str
+    service: str
+    web: str
+
+class User(BaseModel):
+    nick_name: str
+    self_intro: str
+    profile_photo: str
+    comments_id: List[str]
+    post_id: List[str]
+    restaruants_id: List[str]
+    preference: List[float]
