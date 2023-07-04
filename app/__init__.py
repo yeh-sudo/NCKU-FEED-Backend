0de27daa86b8b6384642cdeb64ee6bff336e823d
yeh-sudo <yehchanshen@gmail.com>
2023-07-04 18:24:59 +0800

    add flask restful api

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
+from flask_restful import Api, Resource
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
 app = Flask(__name__)
+api = Api(app)
 client = MongoClient(os.getenv("MONGO_URI"))
 nckufeed_db = client.nckufeed
 
 from app import index
+from app import recommender
