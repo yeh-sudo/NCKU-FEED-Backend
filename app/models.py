99f304cb986acaf2772297746c24f2005c2a5668
JoeySmith1103 <stu30405@gmail.com>
2023-07-11 23:42:25 +0800

    first update on database models

--- app/models.py
+++ app/models.py
@@ -1,33 +1,40 @@
 from pydantic import BaseModel, Field
-from typing import List
+from typing import List, Optional
 
 class Rating(BaseModel):
-    cleanliness: int = Field(...)
-    service: int = Field(...)
-    deliciousness: int = Field(...)
-    CPR: int = Field(...)
-    overall: int = Field(...)
+    cleanliness: int = Field(default=0, ge=0)
+    service: int = Field(default=0, ge=0)
+    deliciousness: int = Field(default=0, ge=0)
+    CPR: int = Field(default=0, ge=0)
+    overall: int = Field(default=0, ge=0)
     
 class Comment(BaseModel):
     rating: Rating
-    content: str = Field(...)
+    content: str
 
 class Restaurant(BaseModel):
     name: str
-    comments_id: List[str]
-    star: float
-    tags: str
-    open_hour: str
-    address: str
-    phone_number: str
-    service: str
-    web: str
+    comments_id: List[str] = Field(default_factory=list)
+    star: float = Field(default=0, ge=0)
+    tags: List[str] = Field(default_factory=list)
+    open_hour: str = None
+    address: str = None
+    phone_number: str = None
+    service: str = None
+    web: str = None
 
 class User(BaseModel):
     nick_name: str
-    self_intro: str
-    profile_photo: str
-    comments_id: List[str]
-    post_id: List[str]
-    restaurants_id: List[str]
-    preference: List[float]
+    self_intro: Optional[str]
+    profile_photo: Optional[str]
+    comments_id: List[str] = Field(default_factory=list)
+    post_id: List[str] = Field(default_factory=list)# 若原本沒有，則預設為空的list
+    restaurants_id: List[str] = Field(default_factory=list)
+    preference: List[float] = Field(default_factory=list)
+
+class Post(BaseModel):
+    content: str
+    picture: Optional[List[str]]
+    restaurants_id: str
+    like: int = Field(default=0, ge=0)
+    comment_id: List[str] = Field(default_factory=list)
