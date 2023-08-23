7c8d7a69eebd847547ee1176dcb10ac33dd85435
JoeySmith1103 <stu30405@gmail.com>
2023-08-23 18:22:26 +0800

    add restaurant's photo attribute and relative functions

--- app/models.py
+++ app/models.py
@@ -29,6 +29,7 @@ class Restaurant(BaseModel):
 
     restaurant_id: Optional[str] = Field(alias="_id")
     name: str
+    photos: List[str] = Field(default_factory=list)
     star: float = Field(default=0, ge=0)
     tags: List[str] = Field(default_factory=list)
     open_hour: List[str] = Field(default_factory=list)
