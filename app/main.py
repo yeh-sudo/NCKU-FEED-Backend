e2c6ecfd46f584ecccff471f2582561073608db9
yeh-sudo <yehchanshen@gmail.com>
2023-10-19 16:36:05 +0800

    add CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 """main module"""
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from .routers import auth
 
 load_dotenv()
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 app.include_router(auth.router)
